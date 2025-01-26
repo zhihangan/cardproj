6fed4855f92bd4d0834534260c56225c707bf7f7
obinnanprogammed <obinnanwakwue10@gmail.com>
2025-01-25 19:58:21 -0600

    background video on Home

--- cardproj-frontend/src/components/Home.tsx
+++ cardproj-frontend/src/components/Home.tsx
@@ -1,12 +1,21 @@
 import { useNavigate } from 'react-router-dom';
 import '../App.css'
+import backgroundVideo from '../assets/video-background.mp4'
+
 function Home() {
     const navigate = useNavigate();
     return (
-        <div className="flex flex-col items-center justify-center">
-            <button onClick={() => navigate("/create-character")} className="bg-black text-white rounded-md p-2 w-fit">Create Character</button>
-            <button onClick={() => navigate("/user-characters")} className="bg-black text-white rounded-md p-2 w-fit">Your Characters</button>
-            <button onClick={() => navigate("/global-characters")} className="bg-black text-white rounded-md p-2 w-fit">Everyone's Characters</button>
+        <div className="relative h-screen flex flex-col items-center justify-center bg-yellow-500">
+            <video autoPlay loop muted className="absolute inset-0 w-full h-full object-cover">
+                <source src={backgroundVideo} type="video/mp4"></source>
+                Your browser doesn't support the video tag.
+            </video>
+            <div className="relative z-10">
+                <button onClick={() => navigate("/create-character")} className="bg-black text-white rounded-md p-2 w-fit">Create Character</button>
+                <button onClick={() => navigate("/user-characters")} className="bg-black text-white rounded-md p-2 w-fit">Your Characters</button>
+                <button onClick={() => navigate("/global-characters")} className="bg-black text-white rounded-md p-2 w-fit">Everyone's Characters</button>
+            </div>
+            
         </div>
     )
 }
