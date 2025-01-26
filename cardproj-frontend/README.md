142fa2d4650926f4df516e73aea3faeeedee29f3
obinnanprogammed <obinnanwakwue10@gmail.com>
2025-01-25 18:00:36 -0600

    updated readme, attempted styling.

--- cardproj-frontend/README.md
+++ cardproj-frontend/README.md
@@ -4,3 +4,4 @@ Before running, run:<br/>
 npm install
 npm install tailwindcss @tailwindcss/postcss react-router-dom
 ```
+To run, call `npm run dev`
--- cardproj-frontend/src/components/Landing.tsx
+++ cardproj-frontend/src/components/Landing.tsx
@@ -2,11 +2,12 @@
  * TODO: Add buttons to background image, create header
  */
 import { useNavigate } from 'react-router-dom';
+import '../App.css';
 
 function Landing() {
     const navigate = useNavigate();
     return (
-        <div>
+        <div className="flex flex-col align-center">
             <h1>FIXME</h1>
             <button onClick={() => navigate("/login")}>Login</button>
         </div>
