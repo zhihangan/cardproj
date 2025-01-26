2e66ce16a9a6e50517f2957b46a3d815e3357dad
obinnanprogammed <obinnanwakwue10@gmail.com>
2025-01-25 18:30:28 -0600

    background image

--- cardproj-frontend/src/components/Landing.tsx
+++ cardproj-frontend/src/components/Landing.tsx
@@ -3,11 +3,12 @@
  */
 import { useNavigate } from 'react-router-dom';
 import '../App.css';
+import background from '../assets/background.png';
 
 function Landing() {
     const navigate = useNavigate();
     return (
-        <div className="flex flex-col align-center">
+        <div style={{ backgroundImage: `url(${background})` }} className="h-screen flex flex-col items-center">
             <h1>FIXME</h1>
             <button onClick={() => navigate("/login")}>Login</button>
         </div>
